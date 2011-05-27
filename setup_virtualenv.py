b522682f4821dfd822b57717ab9995ceab40c20b
rolfst <rolfst@gmail.com>
2011-05-27 10:34:14 +0200

    added virtual env setup when virtualenv and virtualwrapper are installed

---
+++ setup_virtualenv.py
@@ -0,0 +1,18 @@
+#!/bin/env/python
+import os 
+
+postmkenv_text = """proj_name=$(echo $VIRTUAL_ENV|awk -F'/' '{print $NF}')
+mkdir $HOME/projects/python/$proj_name
+add2virtualenv $HOME/projects/python/$proj_name
+cd $HOME/projects/python/$proj_name
+cp ~/dotfiles/developer_requirements.txt $HOME/projects/python/$proj_name/"""
+
+postactivate_txt="""proj_name=$(echo $VIRTUAL_ENV|awk -F'/' '{print $NF}')
+cd ~/projects/python/$proj_name
+pip -r developer_requirements.txt"""
+
+with open(os.path.expanduser("~/.projects/postmkvirtualenv"), 'a') as postmkenv:
+    postmkenv.write(postmkenv_text)
+
+with open(os.path.expanduser("~/.projects/postactivate"), 'a') as activate:
+    activate.write(postactivate_txt)
